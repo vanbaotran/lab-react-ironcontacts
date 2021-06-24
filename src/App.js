274cbc08d3dcdc48071e8d04fcfcdb8532085ca2
massytak <massi.messalti@gmail.com>
2021-06-24 21:40:09 +0200

    2 iteration not done

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ class App extends React.Component {
   state = {
     contacts: json.slice(0,5)
   }
+  addRandomContact =()=>{
+    let random= Math.floor(Math.random()*json.length)
+    let newContact= json[random]
+    let contactCopy= [...this.state.contacts]
+    contactCopy.push(newContact)
+
+    this.setState({
+      contacts: contactCopy
+    })
+
+  }
   render() {
     return (
       <div className="App" >
+         <button onClick={this.addRandomContact}>Add Random Contact </button>
+         
       <table>
         <thead>
           <tr>
@@ -17,7 +30,7 @@ class App extends React.Component {
           <th>Popularity</th>
           </tr>
         </thead>
-        <tbody>
+               <tbody>
           {this.state.contacts.map(el=> { return (
             <tr key={el.id}>
             <td><img src={el.pictureUrl}/></td>
