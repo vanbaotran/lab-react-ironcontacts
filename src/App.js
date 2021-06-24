17f31bb06a4e42612a9cabb1aeadfe81b53447cb
Tran Van <vanbaotran201194@gmail.com>
2021-06-24 21:19:02 +0200

    ite 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import json from './contacts.json'
 
 class App extends React.Component {
+  state = {
+    contacts: json.slice(0,5)
+  }
   render() {
     return (
       <div className="App" >
+      <table>
+        <thead>
+          <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.contacts.map(el=> { return (
+            <tr key={el.id}>
+            <td><img src={el.pictureUrl}/></td>
+            <td>{el.name}</td>
+            <td>{el.popularity}</td>
+          </tr>
+          )  
+          })}
+        </tbody>
+      </table>
       </div>
     );
   }
